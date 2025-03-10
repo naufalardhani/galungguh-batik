af9c9e5991ad407de0ceb30228d093e65099bfbd
naufalardhani <naufalardhani89@gmail.com>
2025-03-10 09:06:58 +0700

    add keuangan + chart

---
+++ backend/app/Filament/Resources/KeuanganResource/Pages/ListKeuangans.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\KeuanganResource\Pages;
+
+use App\Filament\Resources\KeuanganResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListKeuangans extends ListRecords
+{
+    protected static string $resource = KeuanganResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
---
+++ backend/app/Models/Keuangan.php
@@ -0,0 +1,18 @@
+<?php
+// filepath: app/Models/Keuangan.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Keuangan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'waktu',
+        'pemasukan',
+        'pengeluaran',
+        'profit',
+    ];
+}
